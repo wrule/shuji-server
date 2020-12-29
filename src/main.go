397a7388857bc6badfafd62ed3041fc49b71a385
jimao <1982775886@qq.com>
2020-12-29 18:28:15 +0800

    初步调通

--- src/main.go
+++ src/main.go
@@ -22,18 +22,6 @@ func main() {
 		s.Emit("reply", "have "+msg)
 	})
 
-	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-		s.SetContext(msg)
-		return "recv " + msg
-	})
-
-	server.OnEvent("/", "bye", func(s socketio.Conn) string {
-		last := s.Context().(string)
-		s.Emit("bye", last)
-		s.Close()
-		return last
-	})
-
 	server.OnError("/", func(s socketio.Conn, e error) {
 		fmt.Println("meet error:", e)
 	})
@@ -45,8 +33,8 @@ func main() {
 	go server.Serve()
 	defer server.Close()
 
-	http.Handle("/socket.io/", server)
-	http.Handle("/", http.FileServer(http.Dir("./asset")))
-	log.Println("Serving at localhost:8000...")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	http.Handle("/", server)
+	// http.Handle("/", http.FileServer(http.Dir("./asset")))
+	log.Println("Serving at localhost:9079...")
+	log.Fatal(http.ListenAndServe(":9079", nil))
 }
